e6a986d379aea5b769abd3071fedaf24d6ef89fe
Jerrys212 <jerryxd212@gmail.com>
2024-01-04 11:44:43 -0600

    first commit

---
+++ src/components/Alerta.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="alerta">
+        <slot></slot>
+    </div>
+</template>
+
+<style>
+.alerta {
+    padding: 1rem;
+    font-weight: 900;
+    text-transform: uppercase;
+    text-align: center;
+    font-size: 1.6rem;
+    background-color: var(--blanco);
+    border-left: 0.5rem solid #b91c1c;
+    color: #b91c1c;
+    margin-bottom: 1.5rem;
+}
+</style>
